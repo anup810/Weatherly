8e03487311c917d2ab6e7a73df07c21b6b268105
Anup Saud <anupsaud10@gmail.com>
2024-07-09 07:56:49 -0400

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Anup Saud on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
